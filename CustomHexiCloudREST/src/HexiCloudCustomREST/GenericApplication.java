6b0451ac9f0f9d66b09bb1e2c943a1ceb6f9dfb6
vaduri <vaduri@VADURI-LAP.in.oracle.com>
2017-02-13 12:36:34 +0530

    Wrapper REST API to call Compute Cloud REST API from HexiCloud JET application

---
+++ CustomHexiCloudREST/src/HexiCloudCustomREST/GenericApplication.java
@@ -0,0 +1,23 @@
+package HexiCloudCustomREST;
+
+import com.hexicloud.HexiCloudCustomREST;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class GenericApplication extends Application {
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<Class<?>>();
+
+        // Register root resources.
+        classes.add(HexiCloudCustomREST.class);
+
+        // Register provider classes.
+
+        return classes;
+    }
+}
